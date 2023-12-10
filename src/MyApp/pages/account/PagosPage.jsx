bee557ed5e0356f637a3c247fd7e1b08fa16b1cc
leopardo <leonardoandrespardo@gmail.com>
2023-12-10 09:15:08 -0300

    order detail

--- src/MyApp/pages/account/PagosPage.jsx
+++ src/MyApp/pages/account/PagosPage.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import { Card, Row, Col } from "react-bootstrap";
+import PagosListComponent from "./components/PagosListComponent";
 const PagosPage = () => {
   return (
     <>
-      <Card>
-        <Card.Body>
-          <Row>
-            <Col>
-              <p>Pagos Page</p>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
+      <PagosListComponent />
     </>
   );
 };
